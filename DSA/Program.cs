a18173eb6edf58c59d1c323b251c73313f8c728e
prajwalgunjal <prajwalgunjal8@gmail.com>
2024-09-22 15:19:24 +0530

    Add project files.

---
+++ DSA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 4, 1, 2, 1, 2 };
+            SingleNumber(arr);
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            // this is use to remove duplicate element from array and it will only print unique one 
+            List<int> a = nums.ToList();
+            a= a.GroupBy(n => n).Where(v => v.Count() == 1).Select(u=> u.Key).ToList();
+            return a[0];
+        }
+    }
+}
