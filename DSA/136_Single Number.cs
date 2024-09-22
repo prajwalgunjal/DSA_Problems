d45dfaf67f1431beac4b35d3689e5316f09082e5
prajwalgunjal <prajwalgunjal8@gmail.com>
2024-09-22 15:23:00 +0530

    Changes

---
+++ DSA/136_Single Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class _136_Single_Number
+    {
+        public static int SingleNumber(int[] nums= null)
+        {
+            int[] arr = { 4, 1, 2, 1, 2 };
+            // this is use to remove duplicate element from array and it will only print unique one 
+            List<int> a = nums.ToList();
+            a = a.GroupBy(n => n).Where(v => v.Count() == 1).Select(u => u.Key).ToList();
+            return a[0];
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -10,16 +10,9 @@ namespace DSA
     {
         public static void Main(string[] args)
         {
-            int[] arr = { 4, 1, 2, 1, 2 };
-            SingleNumber(arr);
+            // problem no 136 Array -Easy
+            _136_Single_Number.SingleNumber();
         }
 
-        public static int SingleNumber(int[] nums)
-        {
-            // this is use to remove duplicate element from array and it will only print unique one 
-            List<int> a = nums.ToList();
-            a= a.GroupBy(n => n).Where(v => v.Count() == 1).Select(u=> u.Key).ToList();
-            return a[0];
-        }
     }
 }
